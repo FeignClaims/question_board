500eb1b9da76ff2550dae8c33b2169d01b3075d2
FeignClaims <www4112379@163.com>
2024-01-28 10:20:57 +0800

    [conf] 设置版本号

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,7 @@
 project = 'C++ 洋葱剥法交流群资料'
 copyright = '2024, FeignClaims'
 author = 'FeignClaims'
+version = 'v1.0.0'
 release = 'v1.0.0'
 
 # -- General configuration ---------------------------------------------------
@@ -36,5 +37,6 @@ language = 'zh_CN'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_js_files = []
 html_search_language = 'zh'
 html_last_updated_fmt = '%Y-%m-%d %H:%M:%S %z'
