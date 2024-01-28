185f06c4f7bbd0584595e5ef5717d4217cf91aea
FeignClaims <www4112379@163.com>
2024-01-28 11:58:53 +0800

    [main] 提示如何放大网页查看图片

---
+++ docs/build/_static/force_reload.js
@@ -0,0 +1,8 @@
+(function () {
+  if (window.localStorage) {
+    if (!localStorage.getItem("firstLoad")) {
+      localStorage["firstLoad"] = true;
+      window.location.reload();
+    } else localStorage.removeItem("firstLoad");
+  }
+})();
