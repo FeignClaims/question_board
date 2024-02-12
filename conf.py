9c8369da81e9b5f26d9011effec646b62bd4e875
FeignClaims <www4112379@163.com>
2024-02-12 12:51:54 +0800

    将文档移动到根目录

--- conf.py
+++ conf.py
@@ -78,7 +78,6 @@ html_theme_options = {
         }
     ],
     "use_sidenotes": True,
-    'path_to_docs': 'docs/',
     'repository_url': 'https://github.com/FeignClaims/question_board',
     'show_nav_level': 0,
     'show_prev_next': False,
